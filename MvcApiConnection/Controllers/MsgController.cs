6f0cd9e9ab98290178209386f8fc0c07a4940912
Bruce <gkm85663@gmail.com>
2019-08-05 01:07:57 +0800

    Finish Get and Post method

--- MvcApiConnection/Controllers/MsgController.cs
+++ MvcApiConnection/Controllers/MsgController.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using MvcApiConnection.Models.Person;
 using System.Web.Http;
 using MvcApiConnection.Models;
 
@@ -11,32 +7,30 @@ namespace MvcApiConnection.Controllers
 {
     public class MsgController : ApiController
     {
-        // GET: api/Msg
+        // GET: Msg
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Msg/5
+        // GET: Msg/5
         public string Get(int id)
         {
-            return "value";
+            return $"You Get id: {id}";
         }
 
-        // POST: api/Msg
-        public void Post([FromBody]string value)
+        // POST: Msg
+        public string Post([FromBody]Person person)
         {
-            var p = new Person();
-            p.Name = "Bruce";
-            p.Height = 172;
+            return ModelState.IsValid && person.IsAdmin() ? "You can access!" : "Access denied.";
         }
 
-        // PUT: api/Msg/5
+        // PUT: Msg/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/Msg/5
+        // DELETE: Msg/5
         public void Delete(int id)
         {
         }
--- MvcApiConnection/Models/Person.cs
+++ MvcApiConnection/Models/Person.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MvcApiConnection.Models
 {
     public class Person
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public int Height { get; set; }
+
+        public bool IsAdmin()
+        {
+            var adminPerson = new Person { Name = "bruce", Height = 173 };
+            return Name == adminPerson.Name && Height == adminPerson.Height;
+        }
     }
 }
